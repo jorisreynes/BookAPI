5a69dbbd02e67114e1423c0b6edeca8a30fbac97
jorisreynes <joris.reynes81@gmail.com>
2023-09-25 09:16:18 +0200

    Deploiement

--- CoursIPI/Program.cs
+++ CoursIPI/Program.cs
@@ -2,6 +2,7 @@ using CoursIPI.Models.Context;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,22 +29,43 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(c =>
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+//    {
+//        Version = "v1",
+//        Title = "Book List",
+//        Description = "A list to save what you have to read",
+//        TermsOfService = new Uri("https://google.com"),
+//        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+//        {
+//            Name = "Joris",
+//            Email = "joris.reynes@campus-igs-toulouse.fr",
+//            Url = new Uri("https://google.com")
+//        }
+//    });
+//});
+
+builder.Services.AddSwaggerGen(options =>
 {
-    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    options.SwaggerDoc("v1", new OpenApiInfo
     {
         Version = "v1",
         Title = "Book List",
         Description = "A list to save what you have to read",
-        TermsOfService = new Uri("https://google.com"),
-        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        Contact = new OpenApiContact
         {
-            Name = "Joris",
-            Email = "joris.reynes@campus-igs-toulouse.fr",
-            Url = new Uri("https://google.com")
+            Name = "Joris Reynes",
         }
     });
 });
+
+
+
+
+
+
+
 
 //builder.Services.AddDbContext<BookContext>(options =>
 //    options.UseSqlServer("BookApiContextConnection"));
@@ -62,6 +84,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
 
 app.UseHttpsRedirection();
 
